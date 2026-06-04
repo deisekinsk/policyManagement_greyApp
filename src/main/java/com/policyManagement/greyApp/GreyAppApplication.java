eab773cdb4de7e7da516d04e7e5b2d478bef83fd
deisekinsk <kinsk@msn.com>
2026-06-04 00:22:24 -0300

    Start project with SpringBoot, SpringWeb, SpringData, H2 Data and Lombok

---
+++ src/main/java/com/policyManagement/greyApp/GreyAppApplication.java
@@ -0,0 +1,13 @@
+package com.policyManagement.greyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/policyManagement/greyApp/GreyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policyManagement.greyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
